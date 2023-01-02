65ffac02fc73c774fb65ba168f026473cb3dcccf
Rohit Bahuguna <rohitbahuguna.4446@gmail.com>
2023-01-02 17:01:19 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import * as SearchEmoji from 'emoji-api';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [emoji, setEmoji] = useState();
+	const [NameOfEmoji, setNameOfEmoji] = useState();
+
+	const getEmojiName = () => {
+		const data = SearchEmoji.get(emoji);
+		const name = data._data.name;
+		setNameOfEmoji(name);
+	};
+	return (
+		<div className="App">
+			<h1> KNOW YOUR EMOJI</h1>
+			<input
+				type="text"
+				onChange={e => setEmoji(e.target.value)}
+				placeholder="past your emoji here "
+			/>
+			<button onClick={() => getEmojiName()}>
+				click here to know you emoji
+			</button>
+			{NameOfEmoji &&
+				<h1 className="emojiName">
+					This is a <span>{NameOfEmoji}</span> emoji
+				</h1>}{' '}
+			<h1>Some of the cool emojis</h1>
+			<div className="emoji">
+				😀 😃 😄 😁 😆 😅 😂 🤣 🥲 🥹 ☺️ 😊 😇 🙂 🙃 😉 😌 😍 🥰 😘 😗 😙 😚 😋
+				😝 😜 🤪 🤨 🧐 🤓 😎 🥸 🤩 🥳 😏 😒 😞 😔 😟 😕 🙁 ☹️ 😣 😖 😫 😩 🥺 😢
+				😮‍💨 😤 😠 😡 🤬 🤯 😳 🥵 🥶 😱 😨 😰 😥 😓 🫣 🤗 🫡 🤔 🫢 🤭 🤫 🤥 😶
+				😶‍🌫️ 😐 😑 😬 🫠 🙄 😯 😦 😧 😮 😲 🥱 😴 🤤 😪 😵 😵‍💫 🫥 🤐 🥴 🤢 🤮
+				🤧 😷 🤒 🤕 🤑 🤠 😈 👿 👹 👺 🤡 💩 👻 💀 ☠️ 👽 👾 🤖 🎃 😺 😸 😹 😻 😼
+				😽 🙀 😿 😾
+			</div>
+			<footer>
+				<p>@ | 2022 | Rohit Bahuguna</p>
+				<div className="horizontal" />
+				<a href="https://twitter.com/rohitba96862397" target="_blank">
+					<i className="footer_icon fab fa-2x fa-twitter" />
+				</a>
+				<a href="https://www.linkedin.com/in/rohit--bahuguna/" target="_blank">
+					<i className="footer_icon fab fa-2x fa-linkedin" />
+				</a>
+				<a href="https://github.com/rohit-bahuguna/" target="_blank">
+					<i className="footer_icon fab fa-2x fa-github" />
+				</a>
+				<a href="https://rohit-bahuguna.netlify.app/" target="_blank">
+					<i class="fa fa-2x fa-home" aria-hidden="true" />
+				</a>
+			</footer>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+const data = [
+    🫶 Heart Hands
+❤️ Red Heart
+😊 Smiling Face with Smiling Eyes
+✨ Sparkles
+🔥 Fire
+🫡 Saluting Face
+🥹 Face Holding Back Tears
+😂 Face with Tears of Joy
+👍 Thumbs Up
+💀 Skull
+🇺🇸 Flag: United States
+🫠 Melting Face
+✔️ Check Mark
+🎄 Christmas Tree
+🎉 Party Popper
+✅ Check Mark Button
+🎁 Wrapped Gift
+😍 Smiling Face with Heart-Eyes
+👀 Eyes
+🥰 Smiling Face with Hearts
+👉 Backhand Index Pointing Right
+😉 Winking Face
+🤔 Thinking Face
+⭐ Star
+😭 Loudly Crying Face
+🤩 Star-Struck
+👇 Backhand Index Pointing Down
+🤍 White Heart
+🥺 Pleading Face
+🥳 Partying Face
+]
